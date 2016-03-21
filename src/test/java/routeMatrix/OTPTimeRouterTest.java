73cbcb018cdb2ec45c503eec57440b70f464da4f
gabriel <gabriel.thunig@web.de>
2016-03-21 17:09:42 +0100

    updated but still in progress

--- src/test/java/routeMatrix/OTPTimeRouterTest.java
+++ src/test/java/routeMatrix/OTPTimeRouterTest.java
@@ -1,9 +1,12 @@
 package routeMatrix;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import junit.framework.TestCase;
 import net.opengis.ows11.validation.OnlineResourceTypeValidator;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.opentripplanner.analyst.batch.Individual;
 import org.opentripplanner.analyst.batch.SyntheticRasterPopulation;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graph.Vertex;
@@ -13,37 +16,16 @@ import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Map;
-import java.util.TimeZone;
+import java.util.*;
 
 public class OTPTimeRouterTest {
 
     private static final Logger log = LoggerFactory.getLogger(OTPTimeRouterTest.class);
-    public static final double EPSILON = 1e-10;
+    private static final double EPSILON = 1e-10;
 
+    @Ignore
     @Test
     public void testMatrixRouting() throws Exception {
-
-        String input_dir = "input/testMatrixRouting/";
-
-        OTPMatrixRouter.buildGraph(input_dir);
-        Graph graph = OTPMatrixRouter.loadGraph(input_dir);
-        assert graph != null;
-
-        log.info("Preparing settings for routing...");
-        final Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        TimeZone timeZone = TimeZone.getTimeZone("America/San_Francisco");
-        df.setTimeZone(timeZone);
-        calendar.setTimeZone(timeZone);
-        try {
-            calendar.setTime(df.parse("2016-02-02"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        calendar.add(Calendar.SECOND, (7*60*57));
-        log.info("Preparing settings for routing finished.");
 
         SyntheticRasterPopulation rasterPop = new SyntheticRasterPopulation();
         rasterPop.top = 33.9538;
@@ -54,14 +36,22 @@ public class OTPTimeRouterTest {
         rasterPop.rows = 2;
         rasterPop.setup();
 
-        Map<String, Vertex> vertices = OTPMatrixRouter.indexVertices(graph, rasterPop);
+        String input_dir = "input/testMatrixRouting/";
 
-        double[][] timeMatrix = OTPMatrixRouter.routeMatrix(graph, calendar, vertices);
-        double[] actuals = new double[timeMatrix.length * timeMatrix[0].length];
-        int counter = 0;
-        for (int i = 0; i < timeMatrix.length; i++) {
-            for (int e = 0; e < timeMatrix[i].length; e++) {
-                actuals[counter++] = timeMatrix[i][e];
+        Iterator<Individual> iterator = rasterPop.iterator();
+        List<Individual> individuals = new ArrayList<>();
+        while (iterator.hasNext()) {
+            individuals.add(iterator.next());
+        }
+        double[] actuals = new double[individuals.size()*individuals.size()];
+
+        Graph graph = OTPMatrixRouter.loadGraph(input_dir);
+
+        for (int i = 0; i < individuals.size(); i++) {
+            for (int e = 0; e < individuals.size(); e++) {
+                Coordinate origin = new Coordinate(individuals.get(i).lat, individuals.get(i).lon);
+                Coordinate destination = new Coordinate(individuals.get(e).lat, individuals.get(e).lon);
+                actuals[i*individuals.size()+e] = OTPMatrixRouter.getSingleRouteTime(graph, origin, destination);
             }
         }
 

1037b9f0035cce7a5bb9c240ebec8eafc23ed400
gabriel <gabriel.thunig@web.de>
2016-03-23 10:07:44 +0100

    Test succeeds again with new slightly adjusted values considering the new system in which a sample is built with minimal different destination vectors(so the result is minimal different too)

--- src/test/java/routeMatrix/OTPTimeRouterTest.java
+++ src/test/java/routeMatrix/OTPTimeRouterTest.java
@@ -23,9 +23,20 @@ public class OTPTimeRouterTest {
     private static final Logger log = LoggerFactory.getLogger(OTPTimeRouterTest.class);
     private static final double EPSILON = 1e-10;
 
-    @Ignore
     @Test
     public void testMatrixRouting() throws Exception {
+
+        final Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        TimeZone timeZone = TimeZone.getTimeZone("America/San_Francisco");
+        df.setTimeZone(timeZone);
+        calendar.setTimeZone(timeZone);
+        try {
+            calendar.setTime(df.parse("2016-02-02"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        calendar.add(Calendar.SECOND, (7*60*55));
 
         SyntheticRasterPopulation rasterPop = new SyntheticRasterPopulation();
         rasterPop.top = 33.9538;
@@ -47,31 +58,32 @@ public class OTPTimeRouterTest {
 
         Graph graph = OTPMatrixRouter.loadGraph(input_dir);
 
+        double[] expecteds = new double[16];
+        expecteds[0] = 0.0;
+        expecteds[1] = 417.0;
+        expecteds[2] = 1642.0;
+        expecteds[3] = 1578.0;
+        expecteds[4] = 1804.0;
+        expecteds[5] = 0.0;
+        expecteds[6] = 1449.0;
+        expecteds[7] = 1385.0;
+        expecteds[8] = 1631.0;
+        expecteds[9] = 2585.0;
+        expecteds[10] = 0.0;
+        expecteds[11] = 2413.0;
+        expecteds[12] = 2477.0;
+        expecteds[13] = 1801.0;
+        expecteds[14] = 1482.0;
+        expecteds[15] = 0.0;
+
         for (int i = 0; i < individuals.size(); i++) {
             for (int e = 0; e < individuals.size(); e++) {
                 Coordinate origin = new Coordinate(individuals.get(i).lat, individuals.get(i).lon);
                 Coordinate destination = new Coordinate(individuals.get(e).lat, individuals.get(e).lon);
-                actuals[i*individuals.size()+e] = OTPMatrixRouter.getSingleRouteTime(graph, origin, destination);
+                actuals[i*individuals.size()+e] = OTPMatrixRouter.getSingleRouteTime(graph, calendar, origin, destination);
             }
         }
 
-        double[] expecteds = new double[16];
-        expecteds[0] = 0.0;
-        expecteds[1] = 366.0;
-        expecteds[2] = 1659.0;
-        expecteds[3] = 1577.0;
-        expecteds[4] = 1780.0;
-        expecteds[5] = 0.0;
-        expecteds[6] = 1493.0;
-        expecteds[7] = 1411.0;
-        expecteds[8] = 1677.0;
-        expecteds[9] = 2569.0;
-        expecteds[10] = 0.0;
-        expecteds[11] = 2447.0;
-        expecteds[12] = 2290.0;
-        expecteds[13] = 1924.0;
-        expecteds[14] = 1499.0;
-        expecteds[15] = 0.0;
         Assert.assertArrayEquals(expecteds, actuals, EPSILON);
 
         log.info("Shutdown");
